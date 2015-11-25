c3837638133b751bd247301b910dcc991cfd8f7f
IvanovEd <eduard.ivanov@kenshoo.com>
2015-11-25 17:28:19 +0200

    final2

---
+++ src/main/java/com/spilna/sprava/init/SpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.spilna.sprava.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Ivanov on 25.11.15.
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
